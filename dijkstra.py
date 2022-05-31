a43c157c12b5fd316c9f7dee0417a0476f522da3
yuxmi <105485109+yuxmi@users.noreply.github.com>
2022-05-31 14:44:57 -0400

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -20,33 +20,39 @@ def main():
     for k, v in pages.items():
         if v == 'Google':
             print('Google', k)
+            # Determines start node number
             start = k
 
     for k, v in pages.items():
         if v == '渋谷':
             print('渋谷', k)
+            # Determines target node number
             target = k
 
     path = dijkstra(start, target, links)
     final_path = []
+    # Converts page numbers into names
     for node in path:
         final_path.append(pages[node])
     return final_path
-
 
 def dijkstra(start, target, links):
 
     # Setup
     inf = float('inf')
+    # Adding all pages with links to unvisited (queue)
     unvisited = set()
     for k,v in links.items():
         unvisited.add(k)
+    # All nodes except start is labelled as inf since we do not know what the smallest distance is
     dist = {}
     for val in unvisited:
         dist[val] = inf
     dist[start] = 0
+    # Parents dict used to trace back path
     parents = {}
 
+    # Loops until hitting base case (target found)
     while len(unvisited) > 0:
         minDist = inf
         for item in unvisited:
@@ -65,10 +71,14 @@ def dijkstra(start, target, links):
 
         unvisited.remove(node)
         
+        # Iterates through all links from the page (node)
         for neighbor in links[node]:
+            # Weight to another page is 1
             newDist = dist[node] + 1
+            # Skips if the neighbor doesn't have links
             if neighbor not in dist:
                 continue
+            # Runs if the newDist is less than current: either inf or a value if neighbor has been run before
             if newDist < dist[neighbor]:
                 dist[neighbor] = newDist
                 parents[neighbor] = node
