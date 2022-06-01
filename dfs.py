c3f55891210eb9418abf9e5ce0ad9c226d80d242
yuxmi <105485109+yuxmi@users.noreply.github.com>
2022-06-01 11:38:18 -0400

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,3 +1,6 @@
+print(main())
+print(small())
+
 def main():
     pages = {}
     links = {}
@@ -19,13 +22,11 @@ def main():
 
     for k, v in pages.items():
         if v == 'Google':
-            print('Google', k)
             # Determines start node
             start = k
     
     for k, v in pages.items():
         if v == '渋谷':
-            print('渋谷', k)
             # Determines target node
             end = k
 
@@ -61,6 +62,41 @@ def isLegal(node, links, visited):
         return True
     else: return False
 
+    
+    
+def small():
+    pages = {}
+    links = {}
 
-if __name__ == '__main__':
-    main()
+    with open('data/pages_small.txt') as f:
+        for data in f.read().splitlines():
+            page = data.split('\t')
+        # page[0]: id, page[1]: title
+            pages[page[0]] = page[1]
+
+    with open('data/links_small.txt') as f:
+        for data in f.read().splitlines():
+            link = data.split('\t')
+        # link[0]: id (from), links[1]: id (to)
+            if link[0] in links:
+                links[link[0]].add(link[1])
+            else:
+                links[link[0]] = {link[1]}
+
+    for k, v in pages.items():
+        if v == 'Google':
+            # Determines start node
+            start = k
+    
+    for k, v in pages.items():
+        if v == '渋谷':
+            # Determines target node
+            end = k
+
+    visited = []
+    path = DFS(start, end, visited, links, pages)
+    final_path = []
+    # Converts nodes into page names
+    for node in path:
+        final_path.append(pages[node])
+    return final_path
